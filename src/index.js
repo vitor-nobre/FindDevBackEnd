d184b057ad46ffc50dadea882dccaeaec8078188
vitor <vitorn2009@hotmail.com>
2020-07-18 14:13:57 -0300

    Dependencia Cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes');
+const routes = require('./routes')
+const cors = require('cors')
+const app = express()
 
-const app = express();
-
-mongoose.connect(,{
+mongoose.connect('mongodb+srv://erbon8:erbon8@cluster0-knvep.mongodb.net/devDb?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true  
-});
-
-app.use(express.json());
-app.use(routes);
-app.listen(3333);
+})
+app.use(cors({ 
+    origin: 'http://localhost:3000'
+}))
+app.use(express.json())
+app.use(routes)
+app.listen(3333)
